ed738bc1d035d3197a5a552d0f1f02289449e113
Zibi95 <sylwester.sob95@gmail.com>
2026-05-17 11:55:53 +0200

    add Lottery and necessary  classes

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_INT = 100;
+    private static final ColorSupplier colorService = new ColorSupplier();
+    private static final Random randomService = new Random();
+
+    public Ball getRandomBall() {
+        return new Ball(colorService.getRandomColor(), randomService.nextInt(MAX_INT + 1));
+    }
+}
